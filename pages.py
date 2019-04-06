b3cf15fbf9102de576fcd671d550d28690c12d10
soukaryag <sg4fz@virginia.edu>
2019-04-06 10:02:07 -0400

    architecture

---
+++ pages.py
@@ -0,0 +1,15 @@
+import os
+import functools
+import re
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+import psycopg2
+
+
+bp = Blueprint('pages', __name__, url_prefix='')
+
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
